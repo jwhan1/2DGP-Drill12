97755ce3032aa36e6eb950fbf31b8b3f470211ea
jwhan1 <96102340+jwhan1@users.noreply.github.com>
2024-11-25 16:42:44 +0900

    복사

---
+++ zombie.py
@@ -0,0 +1,155 @@
+from pico2d import *
+
+import random
+import math
+import game_framework
+import game_world
+from behavior_tree import BehaviorTree, Action, Sequence, Condition, Selector
+import play_mode
+
+
+# zombie Run Speed
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+RUN_SPEED_KMPH = 10.0  # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+# zombie Action Speed
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 10.0
+
+animation_names = ['Walk', 'Idle']
+
+
+class Zombie:
+    images = None
+
+    def load_images(self):
+        if Zombie.images == None:
+            Zombie.images = {}
+            for name in animation_names:
+                Zombie.images[name] = [load_image("./zombie/" + name + " (%d)" % i + ".png") for i in range(1, 11)]
+            Zombie.font = load_font('ENCR10B.TTF', 40)
+            Zombie.marker_image = load_image('hand_arrow.png')
+
+
+    def __init__(self, x=None, y=None):
+        self.x = x if x else random.randint(100, 1180)
+        self.y = y if y else random.randint(100, 924)
+        self.load_images()
+        self.dir = 0.0      # radian 값으로 방향을 표시
+        self.speed = 0.0
+        self.frame = random.randint(0, 9)
+        self.state = 'Idle'
+        self.ball_count = 0
+        self.patrol_locations = [(43, 274), (1118, 274), (1050, 494), (575, 804), (235, 991), (575, 804), (1050, 494),(1118, 274)]
+        self.loc_no = 0
+        self.build_behavior_tree()
+
+
+    def get_bb(self):
+        return self.x - 50, self.y - 50, self.x + 50, self.y + 50
+
+
+    def update(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % FRAMES_PER_ACTION
+        # fill here
+        self.bt.run()
+
+    def draw(self):
+        if math.cos(self.dir) < 0:
+            Zombie.images[self.state][int(self.frame)].composite_draw(0, 'h', self.x, self.y, 100, 100)
+        else:
+            Zombie.images[self.state][int(self.frame)].draw(self.x, self.y, 100, 100)
+        self.font.draw(self.x - 10, self.y + 60, f'{self.ball_count}', (0, 0, 255))
+        draw_rectangle(*self.get_bb())
+
+    def handle_event(self, event):
+        pass
+
+    def handle_collision(self, group, other):
+        if group == 'zombie:ball':
+            self.ball_count += 1
+
+
+    #거리 비교
+    def distance_less_than(self, x1, y1, x2, y2, r):
+        distance = (x1-x2) ** 2 + (y1-y2) ** 2
+        return distance < (PIXEL_PER_METER * r) ** 2
+    #tx,ty로 이동
+    def move_slightly_to(self, tx, ty):
+        self.dir = math.atan2(ty-self.y, tx-self.x)
+        distance = RUN_SPEED_PPS * game_framework.frame_time
+        self.x += distance * math.cos(self.dir)
+        self.y += distance * math.sin(self.dir)
+    def move_slightly_from(self, tx, ty):
+        self.dir = math.atan2(self.y-ty, self.x-tx)
+        distance = RUN_SPEED_PPS * game_framework.frame_time
+        self.x += distance * math.cos(self.dir)
+        self.y += distance * math.sin(self.dir)
+    #지정 위치 선정
+    def set_target_location(self, x=None, y=None):
+        if not x or not y:
+            raise ValueError('Location should be given')
+        self.tx, self.ty = x,y
+        return BehaviorTree.SUCCESS
+    #랜덤 위치 선정
+    def set_random_location(self):
+        self.tx, self.ty = random.randint(100, 1280 - 100), random.randint(100, 1024 - 100)
+        return BehaviorTree.SUCCESS
+    #보이와 가까운가
+    def is_boy_nearby(self, distance):
+            if self.distance_less_than(play_mode.boy.x, play_mode.boy.y, self.x, self.y, distance):
+                return BehaviorTree.SUCCESS
+            else:
+                return BehaviorTree.FAIL
+    #공 갯수 비교
+    def compare_ball(self):
+        if self.ball_count >= play_mode.boy.ball_count:
+            return BehaviorTree.SUCCESS#접근
+        else:
+            return BehaviorTree.FAIL#도주
+
+    def move_to(self, r=0.2):
+        self.state='Walk'
+        self.move_slightly_to(self.tx, self.ty)
+        if self.distance_less_than(self.tx, self.ty, self.x, self.y, r):
+            return BehaviorTree.SUCCESS
+        else: return BehaviorTree.RUNNING
+
+    def move_to_boy(self, r=0.5):
+        self.state = 'Walk'
+        self.move_slightly_to(play_mode.boy.x, play_mode.boy.y)
+        if self.distance_less_than(play_mode.boy.x, play_mode.boy.y, self.x, self.y, r):
+            return BehaviorTree.SUCCESS
+        else: return BehaviorTree.RUNNING
+    
+    def move_from_boy(self, r=0.5):
+        self.state = 'Walk'
+        self.move_slightly_from(play_mode.boy.x, play_mode.boy.y)
+        if self.distance_less_than(play_mode.boy.x, play_mode.boy.y, self.x, self.y, r):
+            return BehaviorTree.SUCCESS
+        else: return BehaviorTree.RUNNING
+        
+    def build_behavior_tree(self):
+        a1 = Action('이동하기', self.move_to)
+        a2 = Action('방황하기', self.set_random_location)
+        a3 = Action('소년한테 접근', self.move_to_boy)
+        a4 = Action('소년한테 도주', self.move_from_boy)
+
+        c1 = Condition('소년이 근처에 있는가?', self.is_boy_nearby, 7)
+        c2 = Condition('소년의 공이 더 적은가?', self.compare_ball)
+        #방황
+        root = move_to_target_location = Sequence('목표 지점으로 이동하기', a2, a1)
+        #추적
+        root = chase_boy = Sequence('소년을 추적', c2, a3)
+        #도주
+        root = chase_or_avoid = Selector('추적 또는 도주', chase_boy, a4)
+
+        root = close_to_boy = Sequence('소년과 근접', c1, chase_or_avoid)
+        
+        root = chase_or_flee = Selector('추적/도주 또는 순찰', close_to_boy, move_to_target_location)
+
+        self.bt = BehaviorTree(root)
